16f0704ccf858b5ebd7d19196aeb7364c36879a0
JuanOlivas13 <juan11_perez@hotmail.com>
2018-11-05 13:41:54 -0700

    Agregar angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,26 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { GamesPage } from '../pages/games/games';
 import { StatsPage } from '../pages/stats/stats';
 import { SettingsPage } from '../pages/settings/settings';
+
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyBdtVNeCmkpZqkKIW35PwRjpqQ6YPD1nJg",
+  authDomain: "cursoionic-dd5ad.firebaseapp.com",
+  databaseURL: "https://cursoionic-dd5ad.firebaseio.com",
+  projectId: "cursoionic-dd5ad",
+  storageBucket: "cursoionic-dd5ad.appspot.com",
+  messagingSenderId: "810124328266"
+};
 
 @NgModule({
   declarations: [
@@ -22,7 +36,10 @@ import { SettingsPage } from '../pages/settings/settings';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -36,6 +53,7 @@ import { SettingsPage } from '../pages/settings/settings';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
